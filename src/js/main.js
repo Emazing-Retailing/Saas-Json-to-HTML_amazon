23b6c87ca9b1df323ad0f16c5e9d78a745d0f941
Dmitriy Chekulaiev <dmitriy@emazing-retailing.com>
2024-10-08 17:45:57 +0100

    rating

--- src/js/main.js
+++ src/js/main.js
@@ -21,6 +21,8 @@ function displayProduct(productDiv, product) {
     const oldPriceHTML = product.price.old_price ? `<span class="old-price"><del>${product.price.old_price}</del></span>` : '';
     const currentPriceHTML = product.price.price ? `<span class="current-price">${product.price.price}</span>` : '';
 
+
+
     return productDiv.innerHTML = `
                 <div class="product" style="order: ${product.order}">
                     <div class="product-img">
@@ -29,7 +31,7 @@ function displayProduct(productDiv, product) {
                     <div class="product-content">
                         <div class="product-content-title">${product.name}</div>
 <!--                        <div class="product-content-brand"></div>-->
-                        <div class="product-content-rating">${product.rating} <span class="rating rating-${product.rating}"></span><span class="nb-reviews">(${product.nb_reviews})</span></div>
+                        <div class="product-content-rating">${product.rating} <span class="rating-stars rating-stars-n${product.rating}"></span><span class="nb-reviews">(${product.nb_reviews})</span></div>
                         <div class="product-content-price">${currentPriceHTML} ${oldPriceHTML}</div>
                     </div>
                 </div>
