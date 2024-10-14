1e205736580118f80f5a0903626afa4282f48b02
Dmitriy Chekulaiev <dmitriy@emazing-retailing.com>
2024-10-14 12:28:08 +0100

    e

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,47 @@
 const productsContainer = document.getElementById('products');
 
+// Parse json data
+///////////////////////////////////////////////////////////////////////////////
 fetch('products.json')
     .then(response => response.json())
     .then(jsonData => {
 
-        displayJsonInfo(jsonData);
-
+        displayCountProductsInfo(jsonData);
         jsonData.forEach(product => {
-            const productDiv = document.createElement('div');
-            productDiv.classList.add('product-wrap');
+            if (product.price.price) {
+                const productDiv = document.createElement('div');
+                productDiv.classList.add('product-wrap');
 
-            productDiv.innerHTML = displayProduct(productDiv, product);
+                productDiv.innerHTML = displayProduct(productDiv, product);
+                productsContainer.appendChild(productDiv);
+            }
 
-            productsContainer.appendChild(productDiv);
         });
     }).catch(error => console.error('Error fetching JSON:', error));
 
+
+// Display product
+///////////////////////////////////////////////////////////////////////////////
 function displayProduct(productDiv, product) {
-    const oldPriceHTML = product.price.old_price ? `<span class="old-price"><del>${roundToTwoDecimals(product.price.old_price)}</del></span>` : '';
-    const currentPriceHTML = product.price.price ? `<span class="current-price">${roundToTwoDecimals(product.price.price)}</span>` : '';
-    let rating = ratingAlign(roundToHalfStep(product.rating) + '');
-
-
 
     return productDiv.innerHTML = `
-                <div class="product" style="order: ${product.order}">
+                <div class="product">
                     <div class="product-img">
                         <img src="${product.main_image_url}" alt="">
                     </div>
                     <div class="product-content">
                         <div class="product-content-title">${product.name}</div>
-<!--                        <div class="product-content-brand"></div>-->
-                        <div class="product-content-rating product-content-rating-r${product.rating}">${product.rating} <span class="rating-stars rating-stars-n${rating}"></span><span class="nb-reviews nb-reviews-${product.nb_reviews}">(${product.nb_reviews})</span></div>
-                        <div class="product-content-price">${currentPriceHTML} ${oldPriceHTML}</div>
+                        <div class="product-content-brand">Brand</div>
+                        <div class="product-content-rating product-content-rating-${ratingAlign(roundToHalfStep(product.rating) + '')}">${product.rating} <span class="rating-stars rating-stars-${ratingAlign(roundToHalfStep(product.rating) + '')}"></span><span class="nb-reviews nb-reviews-${product.nb_reviews}">(${product.nb_reviews})</span></div>
+                        <div class="product-content-price">${getCurrentPriceHtml(product)} ${getOldPriceHtml(product)}</div>
                     </div>
                 </div>
             `;
 }
 
-function displayJsonInfo(jsonData) {
+// Helpers
+///////////////////////////////////////////////////////////////////////////////
+function displayCountProductsInfo(jsonData) {
     let countElement = document.getElementById('current-qty-products');
     if (countElement) {
         countElement.textContent = jsonData.length;
@@ -58,6 +61,14 @@ function roundToTwoDecimals(price) {
     return price.toFixed(2);
 }
 
-function ratingAlign(rating){
-    return rating.replace('.', '-');
+function ratingAlign(rating) {
+    return rating.replace('.', '');
+}
+
+function getOldPriceHtml(product) {
+    return `<span class="old-price"><del><span class="currency">$</span>100.99</del></span>`;
+}
+
+function getCurrentPriceHtml(product) {
+    return product.price.price ? `<span class="current-price"><sup><span class="currency">$</span></sup>${roundToTwoDecimals(product.price.price)}</span>` : ''
 }
